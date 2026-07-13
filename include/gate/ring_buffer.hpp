c42edcba72a826bfc87d29082bf1e6a022601329
Raphael Mukondiwa <rtmukondiwa1@gmail.com>
2026-07-13 19:19:19 -0400

    modified variable types

--- include/gate/ring_buffer.hpp
+++ include/gate/ring_buffer.hpp
@@ -6,11 +6,16 @@ template <typename T>
 class RingBuffer{
 
 public:
-    int size, rear, front, capacity;
+    std::size_t size;
+    std::atomic<std::size_t> tail{0};
+    std::atomic<std::size_t> head{0};
+    std::size_t capacity;
+    std::size_t mask; // capacity-1
     std::vector<T> buff;
 
-    RingBuffer(int cap) : size(0), tail(cap-1), head(0), capacity(cap), buff(cap)
+    explicit RingBuffer(std::size_t cap) : size(0), tail(cap-1), head(0), capacity(cap), buff(cap), mask(cap-1)
     {
+        assert((cap&(cap-1)) == 0 && cap>0);
     }
 
     bool push(const T& item)
