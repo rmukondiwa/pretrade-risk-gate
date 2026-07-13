0c97c493448a233c9d2983237564c0371c0ff61b
Raphael Mukondiwa <rtmukondiwa1@gmail.com>
2026-07-12 21:49:57 -0400

    First iteration of the ring buffer

---
+++ include/gate/ring_buffer.hpp
@@ -0,0 +1,39 @@
+template <typename T>
+class RingBuffer{
+
+    int size, rear, front, capacity;
+    vector<T> buff;
+
+    RingBuffer<T>(int cap) : size(0), rear(cap-1), front(0), capacity(cap), buff(cap)
+    {
+    }
+
+    bool push(const T& item)
+    {
+        if(isFull())
+        {
+            return false;
+        }
+        rear = (rear +1)&(capacity-1);
+        buff[rear] = item;
+        size++;
+        return true;
+    }
+
+    bool pop(const T& item)
+    {
+        if(size==0)
+        {
+            return false;
+        }
+
+        front = (front+1)&capacity;
+        size--;
+        return true;
+    }
+
+    bool isFull()
+    {
+        return size == capacity;
+    }
+}
