c4288239195278e7cc9ff8a9704ac3ca36235649
Raphael Mukondiwa <rtmukondiwa1@gmail.com>
2026-07-13 20:10:30 -0400

    bug fixes

--- include/gate/ring_buffer.hpp
+++ include/gate/ring_buffer.hpp
@@ -1,28 +1,29 @@
+#pragma once
 #include <vector>
 #include <atomic>
 #include <cstddef>
+#include <cassert>
 template <typename T>
 
 class RingBuffer{
 
 public:
-    std::size_t size;
     std::atomic<std::size_t> tail_{0};
     std::atomic<std::size_t> head_{0};
-    std::size_t capacity;
-    std::size_t mask; // capacity-1
+    std::size_t capacity_;
+    std::size_t mask_; // capacity-1
     std::vector<T> buff;
 
-    explicit RingBuffer(std::size_t cap) : size(0), tail_(cap-1), head_(0), capacity(cap), buff(cap), mask_(cap-1)
+    explicit RingBuffer(std::size_t cap) : tail_(0), head_(0), capacity_(cap), mask_(cap-1), buff(cap)
     {
         assert((cap&(cap-1)) == 0 && cap>0);
     }
 
     bool push(const T& item)
     {
-        std::size_t tail = tail.load(std::memory_order_relaxed); // I own tail, I can read it without synchronization
-        std::size_t next = (tail+1)&mask;
-        if(next == head_.load(std::memory_order_acquire)); // if the next position is the head(the consumer is reading from it), then buffer is full.
+        std::size_t tail = tail_.load(std::memory_order_relaxed); // I own tail, I can read it without synchronization
+        std::size_t next = (tail+1)&mask_;
+        if(next == head_.load(std::memory_order_acquire)) // if the next position is the head(the consumer is reading from it), then buffer is full.
         {
             return false;
         }
@@ -34,13 +35,13 @@ public:
     bool pop(T& out)
     {
         std::size_t head = head_.load(std::memory_order_relaxed); // I own head, I can read it without synchronization
-        std::size_t next = (head+1)&mask;
-        if(head == tail_.load(std::memory_order_aquire)); // if the head is the same as tail, then buffer is empty.
+        std::size_t next = (head+1)&mask_;
+        if(head == tail_.load(std::memory_order_acquire)) // if the head is the same as tail, then buffer is empty.
         {
             return false;
         }
         
-        out = buff[front]; // read the item from the buffer
+        out = buff[head]; // read the item from the buffer
         head_.store(next, std::memory_order_release); // raise the flag to indicate that the item has been consumed and the producer can write to this position again.
         return true;
     }
