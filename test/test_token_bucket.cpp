a51b2f5628ae8b15578d4af9f853ccf69685f622
Raphael Mukondiwa <rtmukondiwa1@gmail.com>
2026-08-29 19:43:32 -0400

    added second test

--- test/test_token_bucket.cpp
+++ test/test_token_bucket.cpp
@@ -15,3 +15,18 @@ void test_starts_full_and_caps()
     }
     assert(!b.try_take(0) && "11th take at same time should fail (empty)");
 }
+
+void test_refill_over_time()
+{
+    TokenBucket b{100, 10}; 
+
+    // drain it
+    for(int i =0; i<10; i++)
+    {
+        assert(b.try_take(0));
+    }
+    assert(!b.try_take(0)); //empty
+
+    // advance 10ms -> ~1 token should have refilled
+    assert(b.try_take(10'000'000) && "should refill after 10 ms");
+}
