d58f24bfc10f17a106afa30a46e911954bddb6c5
Raphael Mukondiwa <rtmukondiwa1@gmail.com>
2026-08-29 19:20:08 -0400

    calling the gate instead of doing push and print

--- bench/replay.cpp
+++ bench/replay.cpp
@@ -15,6 +15,7 @@ It:
 #include "loader.hpp"
 #include "gate/types.hpp"
 #include "gate/ring_buffer.hpp"
+#include "gate/risk_gate.hpp"
 
 class ReplayProducer
 {
@@ -27,18 +28,35 @@ int main()
     ReplayProducer producer;
     RingBuffer<gate::Order> ringbuff(1024);
     auto orders = loader.load("data/orders.csv");
+    gate::RiskConfig cfg{/*max order qty*/ 1000};
+    gate::TokenBucket bucket{/* rate*/ 100, /*capacity*/100 };
+    gate::RiskGate gate{cfg, bucket};
 
+    std::size_t accepted, rejected = 0;
     for(const auto& order : orders)
     {
-        std::cout << order.ts << "\t"
-                  << order.symbol_id << "\t"
-                  << (order.side == gate::Side::Buy ? "Buy" : "Sell") << "\t"
-                  << order.price << "\t"
-                  << order.qty << std::endl;
-        
-        ringbuff.push(order);
-        std::cout << "order pushed!" << std::endl;
-    }
+        gate::Decision d = gate.process(order, order.ts);
 
+        if(d.verdict == gate::Verdict::Accept)
+        {
+            ++accepted;
+        }
+        else
+        {
+            ++rejected;
+             std::cout << "REJECT " << order.symbol_id
+                  << " qty=" << order.qty
+                  << " reason=" << static_cast<int>(d.reason) << "\n";
+        }
+        // std::cout << order.ts << "\t"
+        //           << order.symbol_id << "\t"
+        //           << (order.side == gate::Side::Buy ? "Buy" : "Sell") << "\t"
+        //           << order.price << "\t"
+        //           << order.qty << std::endl;
+        
+        // ringbuff.push(order);
+        // std::cout << "order pushed!" << std::endl;
+    }
+    std::cout << "accepted: " << accepted << "  rejected: " << rejected << "\n";
     return 0;
 }
