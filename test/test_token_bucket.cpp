5005181563e3d046fcdc1fa578bb8a048ec8cffc
Raphael Mukondiwa <rtmukondiwa1@gmail.com>
2026-08-29 19:41:13 -0400

    adding adding first test

--- test/test_token_bucket.cpp
+++ test/test_token_bucket.cpp
@@ -4,3 +4,14 @@
 
 using namespace gate;
 
+void test_starts_full_and_caps()
+{
+    TokenBucket b{100, 10}; //rate 100/sec, capacity 10
+
+    // Should allow exactly 10 takes at the same instant (t=0), then fail
+    for(int i = 0; i<10; i++)
+    {
+        assert(b.try_take(0) && "should allow up to capacity");
+    }
+    assert(!b.try_take(0) && "11th take at same time should fail (empty)");
+}
