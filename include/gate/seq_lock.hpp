a1ff025d637f4149005048f04e0c05e27d92daf2
Raphael Mukondiwa <rtmukondiwa1@gmail.com>
2026-09-07 14:12:06 -0400

    class base structure

--- include/gate/seq_lock.hpp
+++ include/gate/seq_lock.hpp
@@ -1,3 +1,23 @@
 #pragma once
 #include "gate/types.hpp"
 #include <atomic>
+
+namespace gate{
+
+    template <typename T>
+    class SeqLock
+    {
+        private:
+            std::atomic<std::uint64_t> seq; //the seq counter
+            T value;                        // the protected data
+
+        public:
+            SeqLock() : seq(0)
+            {
+
+            }
+
+            void write(const& T& val); // writer thread calls this
+            T read() const;            // reader thread(s) call this
+    };
+}
